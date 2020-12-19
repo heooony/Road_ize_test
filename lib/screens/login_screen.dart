be1384a56b33f7119f685eab4de711e71b9b7dde
oueya1479 <68142821+oueya1479@users.noreply.github.com>
2020-12-20 01:56:02 +0900

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,39 +11,49 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  @override
-  void initState() {
-    super.initState();
+  Duration get loginTime => Duration(milliseconds: 2250);
+
+  Future<String> _authUser(LoginData data) async {
+    try {
+      final user = await _auth.signInWithEmailAndPassword(
+          email: data.name, password: data.password);
+    } catch (e) {
+      return 'User not exists';
+    }
   }
 
-  Future<String> isCurrentUser(LoginData data) async {
+  Future<String> _authCreateUser(LoginData data) async {
     try {
-      final user = (await _auth.signInWithEmailAndPassword(
-              email: data.name, password: data.password))
-          .user;
-    } catch (e) {}
+      final user = await _auth.createUserWithEmailAndPassword(
+          email: data.name, password: data.password);
+      _authUser(data);
+    } catch (e) {
+      return 'Error : $e';
+    }
   }
 
-  Future<String> createUser(LoginData data) async {
-    try {
-      final newUser = (await _auth.createUserWithEmailAndPassword(
-              email: data.name, password: data.password))
-          .user;
-    } catch (e) {}
-  }
+  // Future<String> _recoverPassword(String name) {
+  //   print('Name: $name');
+  //   return Future.delayed(loginTime).then((_) {
+  //     if (!users.containsKey(name)) {
+  //       return 'Username not exists';
+  //     }
+  //     return null;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return FlutterLogin(
-      title: 'ROAD_IZE',
-      logo: 'Thanks',
-      onLogin: isCurrentUser,
-      onSignup: createUser,
-      onSubmitAnimationCompleted: () {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) => BottomNavigation(),
-        ));
-      },
-    );
+        title: 'ROAD_IZE',
+        logo: 'Thanks',
+        onLogin: _authUser,
+        onSignup: _authCreateUser,
+        // onRecoverPassword: _recoverPassword,
+        onSubmitAnimationCompleted: () {
+          Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (context) => BottomNavigation(),
+          ));
+        });
   }
 }
