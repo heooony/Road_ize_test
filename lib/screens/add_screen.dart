eeecaafcbc5c6710da472b5789d060d4ad4afdd2
Dongheon, Kim <oueya1479@naver.com>
2020-12-31 18:26:30 +0900

    Update style to add screen

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:road_ize/services/user_stream.dart';
 import 'package:road_ize/utilities/firebase_information.dart';
-import 'package:road_ize/utilities/snackBar.dart';
+import 'package:road_ize/utilities/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class AddScreen extends StatefulWidget {
   @override
@@ -12,92 +12,120 @@ class _AddScreenState extends State<AddScreen> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final titleController = TextEditingController();
   final introController = TextEditingController();
-  String title;
-  String intro;
+  String title = '';
+  String intro = '';
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = width - 40.0;
     return Scaffold(
+      backgroundColor: mainColor1,
       key: scaffoldKey,
-      body: Container(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 80.0, horizontal: 20.0),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
           child: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          children: [
-            Row(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  '제목 : ',
-                  style: TextStyle(
-                    fontSize: 30.0,
-                  ),
-                ),
-                Flexible(
-                    child: TextField(
+                TextField(
                   controller: titleController,
                   onChanged: (value) {
-                    title = value;
+                    setState(() {
+                      title = value;
+                    });
                   },
-                  style: TextStyle(fontSize: 30.0, color: Colors.black),
                   decoration: InputDecoration(
-                      hintText: '테마 이름을 적어주세요', border: OutlineInputBorder()),
-                )),
-              ],
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.baseline,
-              children: [
-                Text(
-                  '설명 : ',
-                  style: TextStyle(fontSize: 30.0),
+                      border: OutlineInputBorder(), labelText: 'TITLE'),
                 ),
-                Expanded(
-                    child: TextField(
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
                   controller: introController,
                   onChanged: (value) {
                     intro = value;
                   },
                   maxLines: 3,
-                  style: TextStyle(fontSize: 30.0),
                   decoration: InputDecoration(
-                      hintText: '테마 설명을 적어주세요', border: OutlineInputBorder()),
-                )),
+                      border: OutlineInputBorder(), labelText: 'INTRODUCE'),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                  width: width,
+                  height: height,
+                  padding: EdgeInsets.all(20.0),
+                  child: Center(
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 50.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black26, width: 1.3),
+                      borderRadius: BorderRadius.circular(5.0)),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                FlatButton(
+                  onPressed: () async {
+                    await FirebaseInformation.createThema(
+                            scaffoldKey, title, intro)
+                        .then((value) {
+                      if (value == 0) {
+                        titleController.clear();
+                        introController.clear();
+                      }
+                    });
+                  },
+                  child: Text(
+                    '생성',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  color: subColor2,
+                )
               ],
             ),
-            FlatButton(
-              onPressed: () async {
-                UserStream();
-                await FirebaseInformation.firestore
-                    .collection('user_information')
-                    .doc(FirebaseInformation.user.uid)
-                    .collection('map_information')
-                    .doc(title)
-                    .get()
-                    .then((value) async {
-                  if (value.data() == null) {
-                    await FirebaseInformation.firestore
-                        .collection('user_information')
-                        .doc(FirebaseInformation.user.uid)
-                        .collection('map_information')
-                        .doc(title)
-                        .set({'title': title, 'intro': intro});
-                    titleController.clear();
-                    introController.clear();
-                  } else {
-                    MySnackBar(scaffoldKey: scaffoldKey, text: '해당 제목이 존재합니다.')
-                        .snackBarUp();
-                  }
-                });
-              },
-              child: Text('제출'),
-              color: Colors.greenAccent,
-            )
-          ],
+          ),
         ),
-      )),
+      ),
+    );
+  }
+}
+
+class Palette extends StatelessWidget {
+  List<Color> colorList = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.blueAccent,
+    Colors.purple
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+            child: GestureDetector(
+          onTap: () {},
+          child: Container(
+            height: 20.0,
+            color: colorList[0],
+          ),
+        ))
+      ],
     );
   }
 }
